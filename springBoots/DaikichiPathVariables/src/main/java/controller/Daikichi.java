8fc2716e0dbf1807937d74073e2a5ff9d9ec573a
khalid hassouna <khalidhassouna1995@gmail.com>
2023-05-06 01:06:06 +0300

    add

---
+++ springBoots/DaikichiPathVariables/src/main/java/controller/Daikichi.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Daikichi {
+
+@RequestMapping("/daikichi/travel/{city}")	
+public String display(@PathVariable("city") String name){
+	return  "Congratulations! You will soon travel to kyoto!"+name;
+}
+@RequestMapping("/daikichi/lotto/{x}")
+public String luck(@PathVariable("x") int y) {
+	if (y%2==1) {
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends." ;
+	}
+	else {
+		return "ou will take a grand journey in the near future, but be weary of tempting offers" ;
+	}
+	
+}
+}
---
+++ springBoots/DaikichiRoutes/src/test/java/com/DaikichiRoutes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DaikichiRoutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
